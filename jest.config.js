0b10537170b9e23c05da8299bbdb9afec4df7f48
Helder Burato Berto <helder.burato@gmail.com>
2020-12-03 20:26:18 -0300

    chore: configure jest
    It enables jest to test algorithms

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/'],
+};
